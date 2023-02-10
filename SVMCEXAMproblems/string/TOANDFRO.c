04c3e87ae9f6c8e6778575e42876e2d7feedf0a1
Suz <duonghuy14102000@gmail.com>
2023-02-10 17:29:54 +0700

    Finished

--- SVMCEXAMproblems/string/TOANDFRO.c
+++ SVMCEXAMproblems/string/TOANDFRO.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-     return 0; }
+    for (int tc = 1; tc; tc++) {
+        int col;
+        scanf(" %d ", &col);
+        if (col == 0) break;
+        char string[200];
+        scanf(" %s ", &string);
+
+        int row = strlen(string) / col;
+        char rearrange[row][col];
+
+        int idx = 0;
+        while (string[idx] != '\0')
+            for (int r = 0; r < row; r++) {
+                if (r % 2 == 0)
+                    for (int c = 0; c < col; c++) {
+                        rearrange[r][c] = string[idx++];
+                    }
+                else
+                    for (int c = col - 1; c >= 0; c--) {
+                        rearrange[r][c] = string[idx++];
+                    }
+            }
+
+        idx = 0;
+        for (int c = 0; c < col; c++) {
+            for (int r = 0; r < row; r++) {
+                string[idx++] = rearrange[r][c];
+            }
+        }
+
+        printf("%s\n", string);
+    }
+    return 0;
+}
