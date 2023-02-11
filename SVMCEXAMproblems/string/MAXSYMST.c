c0a995f041024706a34afdded2f074bb0a6090af
Suz <duonghuy14102000@gmail.com>
2023-02-11 19:15:52 +0700

    Clean up

--- SVMCEXAMproblems/string/MAXSYMST.c
+++ SVMCEXAMproblems/string/MAXSYMST.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxPalindrome(char s[], int len) {
-    int ans = 1;
-    int left, right;
-    for (int i = 0; i < len; i++) {
-        left = i;
-        right = i;
-        while (right < len && s[right] == s[right + 1]) {
-            right++;
-        }
-        i = right + 1;
-        while (left >= 0 && right < len && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        if (right - left + 1 > ans) ans = right - left + 1;
-    }
-
-    return ans;
-}
-
 int longestPalindrome(char *s) {
     char *start, *end;
-    char *p = s, *subStart = s;
+    char *p = s;
     int maxLen = 1;
     while (*p) {
         start = p;
@@ -37,20 +17,10 @@ int longestPalindrome(char *s) {
             start--;
             end++;
         }
-        if (end - start + 1 > maxLen) {
-            maxLen = end - start + 1;
-            subStart = start;
-        }
+        if (end - start + 1 > maxLen) 
+            maxLen = end - start + 1;        
     }
     return maxLen;
-}
-
-int lengthOfString(char s[]) {
-    int i = 0;
-    while (s[i] != '\0') {
-        i++;
-    }
-    return i;
 }
 
 int main() {
@@ -58,13 +28,13 @@ int main() {
     int T;
     scanf(" %d ", &T);
     for (int tc = 1; tc <= T; tc++) {
-        // char *str = malloc(sizeof(char) * 1001);
-        char str[1001];
+        char *str = malloc(sizeof(char) * 1001);
         scanf("%s", str);
 
-        // int ans = maxPalindrome(str, lengthOfString(str));
         int ans = longestPalindrome(str);
-        // free(str);
+
+        free(str);
+        str = NULL;
         printf("#%d %d\n", tc, ans);
     }
 
